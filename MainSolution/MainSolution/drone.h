692164c80f60a28a2a9e72b3e47acba319031508
Lukas Redynk <mr.erdk@gmail.com>
2016-02-11 20:00:15 +0100

    Changes to drone

--- MainSolution/MainSolution/drone.h
+++ MainSolution/MainSolution/drone.h
@@ -2,23 +2,23 @@
 #define _DRONE_H
 
 #include <cmath>
+#include "Main.h"
 
-class Drone
+struct Drone
 {
     int x, y;
-    int dest_x, dest_y;
     int max_load;
     int cur_load;
 
-public:
     Drone(int max_load_): max_load(max_load) { }
-    void load();
-    void deliver();
-    void unload();
-    void wait();
+
     // sets destination for drone
-    void move(int dest_x_, int dest_y_) { dest_x = dest_x_; dest_y = dest_y_; }
-    int availability() const { return (int) floor(sqrt(pow(dest_x - x, 2) + pow(dest_y - y, 2))); }
+    int deliver(int dest_x, int dest_y) {
+        int turns = ceil(sqrt(pow(dest_x - x, 2) + pow(dest_y - y, 2)));
+        x = dest_x;
+        y = dest_y;
+        return turns;
+    }
 };
 
 #endif //_DRONE_H_
