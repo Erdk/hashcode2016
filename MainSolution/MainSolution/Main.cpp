363f887fbacaf14e72149bc710781ff4fbd6fb27
Rafał Czabaj <Rafa Czabaj>
2016-02-11 19:13:14 +0100

    global variables

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -1,9 +1,16 @@
 #include "Main.h"
 #include <iostream>
+using namespace std;
+
+int rows;
+int cols;
+int number_of_drones;
+int turns;
+int max_payload;
 
 int main(int argc, const char* argv[])
 {
-
+	
 	return 0;
 }
 
