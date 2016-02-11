d3e4e0e837501fcf35b85e89579576830d0f1e5b
H3xas <czabajrafal@gmail.com>
2016-02-11 19:57:32 +0100

    Adding product reader and parser

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -14,9 +14,16 @@ int main(int argc, const char* argv[])
 {
 	scanf("%d %d %d %d %d", &rows, &cols, &number_of_drones, &turns, &max_payload);
 	string product_line;
+	string buffer;
 	getline(cin, product_line);
 	
+	stringstream ss(product_line);
 
+	while (ss >> buffer)
+	{
+		products.push_back(Product(std::stoi(buffer)));
+	}
+	
 	return 0;
 }
 
