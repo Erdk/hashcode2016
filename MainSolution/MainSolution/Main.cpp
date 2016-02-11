7a7e7b50e1c66f424e71f828903b29f30c3a9595
Rafał Czabaj <Rafa Czabaj>
2016-02-11 19:06:46 +0100

    adding product and warehouse class

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -1,7 +1,21 @@
 #include "Main.h"
-
+#include <iostream>
 
 int main(int argc, const char* argv[])
 {
+
 	return 0;
 }
+
+class Product
+{
+	public:
+		int weight;
+};
+
+class Warehouse
+{
+	public:
+		int row_x;
+		int col_y;
+};
