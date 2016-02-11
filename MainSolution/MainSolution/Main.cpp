bcdbe151e157daa758443c04da7b8ae56eb2b508
Lukas Redynk <mr.erdk@gmail.com>
2016-02-11 21:46:00 +0100

    Fixes to reading input file

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -43,9 +43,8 @@ void process_customer(Customer c, Drone d)
 
 int main(int argc, const char* argv[])
 {
-	scanf("%d %d %d %d %d", &rows, &cols, &total_drones, &turns, &max_payload);
-
-	scanf("%d", &total_products);
+	scanf("%d %d %d %d %d\n", &rows, &cols, &total_drones, &turns, &max_payload);
+	scanf("%d\n", &total_products);
 	string product_line;
 	string buffer;
 	getline(cin, product_line);
@@ -58,12 +57,12 @@ int main(int argc, const char* argv[])
 		products.push_back(Product(std::stoi(buffer)));
 	}
 	
-	scanf("%d", &total_warehouses);
+	scanf("%d\n", &total_warehouses);
 	for (int it = 0; it < total_warehouses; it++)
 	{
 		int war_x, war_y;
-		scanf("%d %d", &war_x, &war_y);
-		Warehouse* warehouse = new Warehouse(war_x, war_y);
+		scanf("%d %d\n", &war_x, &war_y);
+		Warehouse warehouse = Warehouse(war_x, war_y);
 		string warhouse_prod;
 		getline(cin, warhouse_prod);
 		string war_buffer;
@@ -79,41 +78,40 @@ int main(int argc, const char* argv[])
 			}
 			else
 			{
-				warehouse->add_product(prod_id, products.at(prod_id).weight, count);
+				warehouse.add_product(prod_id, products.at(prod_id).weight, count);
 			}
 			prod_id++;
 		}
+
+        warehouses.push_back(warehouse);
 	}
 
-	scanf("%d", &total_orders);
+	scanf("%d\n", &total_orders);
 
 	for (int it = 0; it < total_orders; it++)
 	{
 		int loc_x, loc_y;
-		scanf("%d %d", &loc_x, &loc_y);
-		Customer* customer = new Customer(loc_x, loc_y);
+		scanf("%d %d\n", &loc_x, &loc_y);
+		Customer customer = Customer(loc_x, loc_y);
 		int number_of_prod_orders;
-		scanf("%d", &number_of_prod_orders);
-		for (int prod_order_it = 0; prod_order_it < number_of_prod_orders; prod_order_it++)
-		{
-			string orders;
-			string order_buffer;
-			getline(cin, orders);
-			stringstream orders_ss(orders);
-			while (orders_ss >> order_buffer)
-			{
-				int prod_id = std::stoi(order_buffer);
-				if (customer->orders.find(prod_id) == customer->orders.end())
-				{
-					customer->orders.emplace(prod_id, 1);
-				}
-				else
-				{
-					customer->orders.at(prod_id)++;
-				}
-			}
-		}
-		customers.push_back(*customer);
+		scanf("%d\n", &number_of_prod_orders);
+        string orders;
+        string order_buffer;
+        getline(cin, orders);
+        stringstream orders_ss(orders);
+        while (orders_ss >> order_buffer)
+        {
+            int prod_id = std::stoi(order_buffer);
+            if (customer.orders.find(prod_id) == customer.orders.end())
+            {
+                customer.orders.emplace(prod_id, 1);
+            }
+            else
+            {
+                customer.orders.at(prod_id)++;
+            }
+        }
+		customers.push_back(customer);
 	}
 
 
