247808576f9fbcd2c3df67bd2a0665826be96214
Lukas Redynk <mr.erdk@gmail.com>
2016-02-11 19:20:26 +0100

    Updated warehouse and product

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -1,5 +1,6 @@
 #include "Main.h"
-#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int rows;
@@ -8,21 +9,28 @@ int number_of_drones;
 int turns;
 int max_payload;
 
-int main(int argc, const char* argv[])
+struct Product
 {
-	
-	return 0;
-}
-
-class Product
-{
-	public:
-		int weight;
+    int weight;
+    int count;
+    Product(int weight_, int count_) : weight(weight_), count(count_) { }
 };
 
 class Warehouse
 {
-	public:
-		int row_x;
-		int col_y;
+public:
+    int x;
+    int y;
+    vector<Product> products;
+
+    Warehouse(int x_, int y_) : x(x_), y(y_) { }
+    void add_product(int weight, int count) {
+        products.push_back(Product(weight, count));
+    }
 };
+
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
+
