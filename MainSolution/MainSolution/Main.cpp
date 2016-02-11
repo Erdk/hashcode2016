e072933ca96bb25c53a48f66130d8245fc285d6f
H3xas <czabajrafal@gmail.com>
2016-02-11 19:42:19 +0100

    input reader v2

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -1,5 +1,6 @@
 #include "Main.h"
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,26 +12,31 @@ int max_payload;
 
 struct Product
 {
-    int weight;
-    int count;
-    Product(int weight_, int count_) : weight(weight_), count(count_) { }
+	int weight;
+	int count;
+	Product(int weight_, int count_) : weight(weight_), count(count_) { }
 };
 
 class Warehouse
 {
 public:
-    int x;
-    int y;
-    vector<Product> products;
+	int x;
+	int y;
+	vector<Product> products;
 
-    Warehouse(int x_, int y_) : x(x_), y(y_) { }
-    void add_product(int weight, int count) {
-        products.push_back(Product(weight, count));
-    }
+	Warehouse(int x_, int y_) : x(x_), y(y_) { }
+	void add_product(int weight, int count) {
+		products.push_back(Product(weight, count));
+	}
 };
 
 int main(int argc, const char* argv[])
 {
+	scanf("%d %d %d %d %d", &rows, &cols, &number_of_drones, &turns, &max_payload);
+	string product_line;
+	getline(cin, product_line);
+
+
 	return 0;
 }
 
