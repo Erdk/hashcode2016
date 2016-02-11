9c4d624ea886ad2a867601e0cdaf19ec3a1be1b9
H3xas <czabajrafal@gmail.com>
2016-02-11 20:00:39 +0100

    ADding total_prod and total_warehouse variables

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int rows;
 int cols;
-int number_of_drones;
+int total_drones;
 int turns;
 int max_payload;
+int total_products = 0;
+int total_warehouses = 0;
 vector<Product> products;
 
 int main(int argc, const char* argv[])
 {
-	scanf("%d %d %d %d %d", &rows, &cols, &number_of_drones, &turns, &max_payload);
+	scanf("%d %d %d %d %d", &rows, &cols, &total_drones, &turns, &max_payload);
 	string product_line;
 	string buffer;
 	getline(cin, product_line);
@@ -21,9 +23,12 @@ int main(int argc, const char* argv[])
 
 	while (ss >> buffer)
 	{
+		total_products++;
 		products.push_back(Product(std::stoi(buffer)));
 	}
 	
+
+
 	return 0;
 }
 
