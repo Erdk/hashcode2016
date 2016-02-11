806cfa7455e7e8dc9d039c2ccff8fcf75fbe8f0c
H3xas <czabajrafal@gmail.com>
2016-02-11 20:12:23 +0100

    warehouse reader

--- MainSolution/MainSolution/Main.cpp
+++ MainSolution/MainSolution/Main.cpp
@@ -11,6 +11,7 @@ int max_payload;
 int total_products = 0;
 int total_warehouses = 0;
 vector<Product> products;
+vector<Warehouse> warehouses;
 
 int main(int argc, const char* argv[])
 {
@@ -27,6 +28,32 @@ int main(int argc, const char* argv[])
 		products.push_back(Product(std::stoi(buffer)));
 	}
 	
+	scanf("%d", &total_warehouses);
+	for (int it = 0; it < total_warehouses; it++)
+	{
+		int war_x, war_y;
+		scanf("%d %d", war_x, war_y);
+		Warehouse* warehouse = new Warehouse(war_x, war_y);
+		string warhouse_prod;
+		getline(cin, warhouse_prod);
+		string war_buffer;
+		stringstream warss(warhouse_prod);
+
+		int prod_id = 0;
+		while (warss >> war_buffer)
+		{
+			int count = std::stoi(war_buffer);
+			if (count <= 0)
+			{
+				continue;
+			}
+			else
+			{
+				warehouse->add_product(products.at(sizeof(Product)*prod_id).weight, count);
+			}
+			prod_id++;
+		}
+	}
 
 
 	return 0;
